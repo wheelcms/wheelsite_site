6dee9ed86d2d07c7fb21027d9f4628d513456b5a
Ivo van der Wijk <github@in.m3r.nl>
2013-03-07 12:23:10 +0100

    frontpage / footer styling

--- wheelsite_site/models.py
+++ wheelsite_site/models.py
@@ -1,5 +1,5 @@
 from wheelcms_axle.templates import template_registry
-from wheelcms_spokes.page import PageType
+from wheelcms_spokes.page import Page, PageType
 from wheelcms_axle.content import ImageContent
 
 def frontpage_context(handler, request, node):
@@ -11,7 +11,11 @@ def frontpage_context(handler, request, node):
     return dict(carousel=[n for n in node.child('data').children()
                           if isinstance(n.content(), ImageContent)
                           and n.content().spoke().workflow().is_visible()
-                         ]
+                         ],
+                slots=[s for s in node.child('data').children()
+                          if isinstance(s.content(), Page)
+                          and s.content().spoke().workflow().is_visible()
+                         ],
                )
 
 
